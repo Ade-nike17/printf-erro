520594ad1855ebba582e36078b957aae22dbd7e5
Ade-nike17 <perfectnyke@gmail.com>
2023-03-26 16:19:58 -0700

    adds putchar file

---
+++ _putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - function prints out a single character
+ *
+ * @c: character to be printed
+ *
+ * Return: On success 1
+ * On error, -1 is returned, errno is set appropriately
+ */
+
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
