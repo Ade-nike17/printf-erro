9d96d2a32e8e0f44d6b921e14430a59c825ca17f
Ade-nike17 <perfectnyke@gmail.com>
2023-03-27 04:40:02 -0700

    updates header file

--- main.h
+++ main.h
@@ -4,5 +4,7 @@
 /* function prototypes */
 int _printf(const char *format, ...);
 int _putchar(char c);
+void write_string(const char *str, int *num);
+void write_char(char c, int *num);
 
 #endif /* MAIN_H */
