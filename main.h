39029dae3d705577a0857110dd09cda9ad7949ad
Ade-nike17 <perfectnyke@gmail.com>
2023-03-28 12:54:00 -0700

    creates a new variable type printer

--- main.h
+++ main.h
@@ -1,10 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+
 /* function prototypes */
 int _printf(const char *format, ...);
 int _putchar(char c);
-void write_string(const char *str, int *num);
-void write_char(char c, int *num);
+int print_string(va_list args);
+int print_char(va_list args);
+
+/**
+ * struct printer - struct for printer functions
+ *
+ * @type: the conversion specifier
+ * @print: the function to print the argument
+ */
+
+typedef struct printer
+{
+	char *type;
+	int (*print)(va_list);
+} printer_t;
 
 #endif /* MAIN_H */
