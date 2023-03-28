c1eb202bc0d3340b6063268918bee9e36ba36ecf
Ade-nike17 <perfectnyke@gmail.com>
2023-03-28 12:52:07 -0700

    produces output according to %c and %s

--- _printf.c
+++ _printf.c
@@ -5,91 +5,52 @@
 #include "main.h"
 
 /**
- * write_char - function prints a single character
- *
- * @c: character to be printed
- * @num: pointer to an int var that tracks number of characters to be printed
- *
- * Return: nothing
- */
-
-/* writes a single char to the output stream */
-void write_char(char c, int *num)
-{
-	if (num != NULL)
-	{
-		_putchar(c);
-		(*num)++;
-	}
-}
-
-/**
- * write_string - writes a null terminated string to the output stream
- *
- * @str: string to be printed
- * @num: pointer to an int var that tracks number of strings to be printed
- *
- * Return: void
- */
-
-void write_string(const char *str, int *num)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (num != NULL)
-		{
-			write_char(str[i], num);
-		}
-	}
-}
-
-
-/**
  * _printf - function produces output according to a format
  *
- * @format: type of format in arg
- * Return: number of characters printed
+ * @format: pointer to string type of format in arg
+ * Return: number of characters printed (excluding the null byte)
  */
 
 int _printf(const char *format, ...)
 {
-	int i, num = 0;
+	int i, j, len = 0;
 
 	va_list args;
 
+	/* printers array of type printer_t containing format specifiers */
+	printer_t printers[] = {{"s", print_string}, {"c", print_char}, {NULL, NULL}};
+
+	if (format == NULL)
+		return (-1);
 	va_start(args, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			if (format[i + 1] == 'c')
-			{
-				char c = va_arg(args, int);
-
-				write_char(c, &num);
-				i++;
-			}
-			else if (format[i + 1] == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				write_string(s, &num);
-				i++;
-			}
-			else if (format[i + 1] == '%')
-			{
-				write_char('%', &num);
-				i++;
-			}
+			_putchar(format[i]), len++;
+		}
+		else if (format[i + 1] == '%')
+		{
+			_putchar('%'), len++, i++;
 		}
 		else
 		{
-			write_char(format[i], &num);
+			for (j = 0; printers[j].type != NULL; j++)
+			{
+				if (format[i + 1] == *(printers[j].type))
+				{
+					len += printers[j].print(args);
+					i++;
+					break;
+				}
+			}
+			if (printers[j].type == NULL)
+			{
+				_putchar(format[i]), len++;
+			}
 		}
 	}
 	va_end(args);
-	return (num);
+	return (len);
 }
